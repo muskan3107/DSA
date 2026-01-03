02fcaf6485e294f7b25c4f21fce0f37215ebca53
muskan3107 <167448632+muskan3107@users.noreply.github.com>
2026-01-04 01:59:41 +0530

    Time: 1 ms (11.63%) | Memory: 46.4 MB (79.27%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -11,7 +11,7 @@ class Solution {
             og[i] = nums[i];
         }
 
-        for (int i = 1; i < 2*n - 1; i++) {
+        for (int i = 0; i < 2*n; i++) {
             if (i % 2 != 0) {
                 nums[i] = temp[i / 2];
             }
