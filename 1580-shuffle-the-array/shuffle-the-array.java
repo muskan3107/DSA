41b751b5214562db2ed7235d35e100e29ac7bb00
muskan3107 <167448632+muskan3107@users.noreply.github.com>
2026-01-04 02:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (7.21%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,25 +1,12 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        n = nums.length/2;
-        int[] temp = new int[n];
-        int[] og = new int[n]; 
-        for (int i = 0; i < n; i++) {
-            temp[i] = nums[n + i];
-        }
+        int[] res = new int[2 * n];
 
         for (int i = 0; i < n; i++) {
-            og[i] = nums[i];
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[n + i];
         }
 
-        for (int i = 0; i < 2*n; i++) {
-            if (i % 2 != 0) {
-                nums[i] = temp[i / 2];
-            }
-            else {
-                nums[i] = og[i / 2];
-            }
-        }
-
-        return nums;
+        return res;
     }
 }
